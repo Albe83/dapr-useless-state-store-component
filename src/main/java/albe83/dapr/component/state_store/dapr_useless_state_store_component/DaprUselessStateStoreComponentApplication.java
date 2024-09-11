e6a734cb168e2984321b034e49742396941f6a18
Alberto Clemente <alberto.clemente@sisal.it>
2024-09-11 15:05:33 +0200

    Getting started

---
+++ src/main/java/albe83/dapr/component/state_store/dapr_useless_state_store_component/DaprUselessStateStoreComponentApplication.java
@@ -0,0 +1,13 @@
+package albe83.dapr.component.state_store.dapr_useless_state_store_component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprUselessStateStoreComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaprUselessStateStoreComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/albe83/dapr/component/state_store/dapr_useless_state_store_component/DaprUselessStateStoreComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package albe83.dapr.component.state_store.dapr_useless_state_store_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaprUselessStateStoreComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
